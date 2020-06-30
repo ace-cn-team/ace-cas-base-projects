2931690eb58e6d0ff00f941bc7ac19272708392b
caspar <279397942@qq.com>
2020-06-30 18:33:14 +0800

    init projects

--- ace-cas-base-define/src/main/java/ace.cas.base.define/model/response/GetOAuth2TokenResponse.java
+++ ace-cas-base-define/src/main/java/ace.cas.base.define/model/response/GetOAuth2TokenResponse.java
@@ -1,15 +1,26 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 package ace.cas.base.define.model.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * @author Caspar
+ * @contract 279397942@qq.com
+ * @create 2020/2/24 2:12
+ * @description
+ */
+@Data
+@Accessors(chain = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetOAuth2TokenResponse {
     private String accessToken;
     private String refreshToken;
     private String tokenType;
     private Integer expiresIn;
     private String scope;
-
 }
